375fa3eba990723dd4e9b37bdf6b8bcdc3a32c4b
Kris <kapsona@yahoo.com>
2020-03-29 08:48:32 -0700

    Updates

---
+++ precache-manifest.96c40dd0311f395826490d9efbe04646.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab01178b6f4e4aff42269c5e2a8335f",
+    "url": "/robocup/index.html"
+  },
+  {
+    "revision": "0654a91307027b0432d9",
+    "url": "/robocup/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c1229e6651ff89681bd5",
+    "url": "/robocup/static/js/2.c16bb05b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robocup/static/js/2.c16bb05b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0654a91307027b0432d9",
+    "url": "/robocup/static/js/main.757b2d3f.chunk.js"
+  },
+  {
+    "revision": "411c6c54444cb5b853ea",
+    "url": "/robocup/static/js/runtime-main.63b3ff99.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robocup/static/media/logo.5d5d9eef.svg"
+  }
+]);
